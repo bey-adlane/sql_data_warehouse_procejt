1b36e8a0b9beb16b428aa6266d6ab77c8bba2337
Adlane_BEY <95702979+bey-adlane@users.noreply.github.com>
2025-12-13 12:34:50 +0100

    Create DDL bronze
    This script creates tables in the 'bronze' schema, dropping existing tables if they exist.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,78 @@
+/*
+Script DDL : Création des tables Bronze
+==================================================================================
+Objectif du script :
+
+Ce script crée des tables dans le schéma « bronze », en supprimant les tables existantes 
+le cas échéant : Exécutez ce script pour redéfinir la structure DDL des tables « bronze ».
+*/
+
+IF OBJECT_ID ('bronze.crm_cust_info', 'u') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info (
+
+	cst_id INT,
+	cst_key NVARCHAR(50),
+	cst_firstname NVARCHAR(50),
+	cst_lastname NVARCHAR(50),
+	cst_marital_status NVARCHAR(50),
+	cst_gndr NVARCHAR(50),
+	cst_create_date DATE
+)
+
+IF OBJECT_ID ('bronze.crm_prd_info', 'u') IS NOT NULL
+	DROP TABLE bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info (
+
+	prd_id INT,
+	prd_key NVARCHAR(50),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+
+)
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'u') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details (
+
+	sls_ord_num NVARCHAR(50),
+	sls_prd_key NVARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+
+)
+
+IF OBJECT_ID ('bronze.erp_cust_az12', 'u') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12 (
+
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen  NVARCHAR(50)
+)
+IF OBJECT_ID ('bronze.erp_loc_a101', 'u') IS NOT NULL
+	DROP TABLE bbronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101 (
+
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+)
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'u') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+
+	id INT,
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
+)
+
+
